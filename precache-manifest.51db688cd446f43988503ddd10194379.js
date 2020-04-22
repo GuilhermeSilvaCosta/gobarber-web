9aa3d96388fabd73e73f5b0e62a51d5f8864bd2d
Guilherme Costa <tec.guilherme.dsc@gmail.com>
2020-04-22 18:32:46 -0300

    Updates

---
+++ precache-manifest.51db688cd446f43988503ddd10194379.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c7223e619dffa95d5a01b94ee70af57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5afee8041e3518b4ac92",
+    "url": "/static/css/2.07a2a3e9.chunk.css"
+  },
+  {
+    "revision": "5afee8041e3518b4ac92",
+    "url": "/static/js/2.6ff7fbde.chunk.js"
+  },
+  {
+    "revision": "d6addd2baa8c25229531b9d715491a4c",
+    "url": "/static/js/2.6ff7fbde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72becc0940a28366de5d",
+    "url": "/static/js/main.ffbcbbea.chunk.js"
+  },
+  {
+    "revision": "aea484d9529cefabcfd0",
+    "url": "/static/js/runtime-main.d841a831.js"
+  },
+  {
+    "revision": "291b6cc6a5ba30f46440c782a41c233c",
+    "url": "/static/media/logo.291b6cc6.svg"
+  },
+  {
+    "revision": "3276c2f605d2cc3f16f185aa8f377120",
+    "url": "/static/media/purple-logo.3276c2f6.svg"
+  }
+]);
